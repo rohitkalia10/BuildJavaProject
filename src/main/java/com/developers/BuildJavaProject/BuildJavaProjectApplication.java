0f66b7d0eb31c98a3c593b32fc9d7af84c274082
Rohit Kalia <kalia.rohit@yahoo.com>
2019-06-20 12:19:46 -0400

    Project finally started....

---
+++ src/main/java/com/developers/BuildJavaProject/BuildJavaProjectApplication.java
@@ -0,0 +1,14 @@
+package com.developers.BuildJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildJavaProjectApplication.class, args);
+		System.out.println("This should start mow...");
+	}
+
+}
---
+++ src/test/java/com/developers/BuildJavaProject/BuildJavaProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.developers.BuildJavaProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuildJavaProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
