141d15cbbb5d18c90e5a667eadd25df42c3953d8
Sanjay Kumar <sanjayk.java@gmail.com>
2019-06-21 01:16:53 -0400

    Added Eureka Client, Registration, Added few dependencies in pom

--- src/main/java/core/BuildJavaProjectApplication.java
+++ src/main/java/core/BuildJavaProjectApplication.java
@@ -3,8 +3,11 @@ package core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = { "core","controller"} ) 
 public class BuildJavaProjectApplication {
 
