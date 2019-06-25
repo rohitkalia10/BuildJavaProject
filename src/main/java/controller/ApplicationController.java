42e63a6754270bdf7d11867234d2e294f9a7069b
Sanjay Kumar <sanjayk.java@gmail.com>
2019-06-24 23:46:11 -0400

    Added GetByName and GetbyPrice Method

--- src/main/java/controller/ApplicationController.java
+++ src/main/java/controller/ApplicationController.java
@@ -9,54 +9,126 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 
 public class ApplicationController {
 
-    @Autowired
-    SpringMongoConfig config;
+	@Autowired
+	SpringMongoConfig config;
 
-    @RequestMapping (value = "/shop" , method = RequestMethod.POST)
-public String onlineShopping() {
+	@RequestMapping(value = "/shop", method = RequestMethod.POST)
+	public String onlineShopping() {
 
-  return "Name and Price for an item";
-}
+		return "Name and Price for an item";
+	}
 
-@RequestMapping (value = "/shopping" , method = RequestMethod.POST)
-    public JSONObject getGeneric(@RequestBody String stringToParse){
+	@RequestMapping(value = "/shopping", method = RequestMethod.POST)
+	public JSONObject getGeneric(@RequestBody String stringToParse) {
 
-        JSONParser parser = new JSONParser();
-        JSONObject json = new JSONObject();
-        try {
-            json = (JSONObject) parser.parse(stringToParse);
+		JSONParser parser = new JSONParser();
+		JSONObject json = new JSONObject();
+		try {
+			json = (JSONObject) parser.parse(stringToParse);
 
-            String name  = json.get("name").toString();
-            String price = json.get("price").toString();
+			String name = json.get("name").toString();
+			String price = json.get("price").toString();
 
-            MongoTemplate mongoTemplate = config.mongoTemplate();
-            Query query = new Query();
-            query.addCriteria(Criteria.where("name").is("Front Door"));
-            List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+			MongoTemplate mongoTemplate = config.mongoTemplate();
+			Query query = new Query();
+			query.addCriteria(Criteria.where("name").is("Front Door"));
+			List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
 
-            System.out.println("Size is" +inventoryList.size());
+			System.out.println("Size is" + inventoryList.size());
 
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    return json;
-    }
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return json;
+	}
 
-    @RequestMapping("/")
-    public String index() {
-    return "Greetings from our developers!";
-}
+	@RequestMapping(value = "/searchByPrice/{price}", method = RequestMethod.GET)
+	public JSONObject searchByPrice(@PathVariable("price") int price) {
+		JSONObject json = new JSONObject();
+		List<Inventory> inventoryList = new ArrayList<Inventory>();
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = null;
+		try {
+
+			MongoTemplate mongoTemplate = config.mongoTemplate();
+			Query query = new Query();
+			query.addCriteria(Criteria.where("price").is(price));
+			inventoryList = mongoTemplate.find(query, Inventory.class);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			jsonString =  mapper.writeValueAsString(inventoryList);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		  
+		  System.out.println("***********Value :::"+jsonString);
+		
+		return json;
+		
+		
+	}
+	
+	@RequestMapping(value = "/searchByName/{name}", method = RequestMethod.GET)
+	public String searchByName(@PathVariable("name") String name) {
+		JSONObject json = new JSONObject();
+		List<Inventory> inventoryList = new ArrayList<Inventory>();
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = null;
+		try {
+
+			MongoTemplate mongoTemplate = config.mongoTemplate();
+			Query query = new Query();
+			query.addCriteria(Criteria.where("name").is(name));
+			inventoryList = mongoTemplate.find(query, Inventory.class);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			jsonString =  mapper.writeValueAsString(inventoryList);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		  
+		  System.out.println("***********Value :::"+jsonString);
+		
+		return jsonString;
+		
+		
+		
+	}
+	
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from our developers!";
+	}
 }
