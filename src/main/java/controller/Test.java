f47e641d110e1f1fc679fd6e69e85bc0a03bb242
Sanjay Kumar <sanjayk.java@gmail.com>
2019-06-25 17:08:38 -0400

    test

---
+++ src/main/java/controller/Test.java
@@ -0,0 +1,84 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import model.Inventory;
+
+public class Test {
+
+	
+	@RequestMapping(value = "/shopping", method = RequestMethod.POST)
+	public JSONObject getGeneric(@RequestBody String stringToParse) {
+
+		JSONParser parser = new JSONParser();
+		JSONObject json = new JSONObject();
+		try {
+			json = (JSONObject) parser.parse(stringToParse);
+
+			String name = json.get("name").toString();
+			String price = json.get("price").toString();
+
+			MongoTemplate mongoTemplate = config.mongoTemplate();
+			Query query = new Query();
+			query.addCriteria(Criteria.where("name").is("Front Door"));
+			List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+
+			System.out.println("Size is" + inventoryList.size());
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return json;
+	}
+
+	@RequestMapping(value = "/searchByPrice/{price}", method = RequestMethod.GET)
+	public String searchByPrice(@PathVariable("price") double price) {
+//		JSONObject json = new JSONObject();
+		List<Inventory> inventoryList = new ArrayList<Inventory>();
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = null;
+		try {
+
+			MongoTemplate mongoTemplate = config.mongoTemplate();
+			Query query = new Query();
+			query.addCriteria(Criteria.where("price").is(price));
+			inventoryList = mongoTemplate.find(query, Inventory.class);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			// jsonString = mapper.writeValueAsString(inventoryList);
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			jsonString = gson.toJson(inventoryList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("***********Value :::" + jsonString);
+
+		return jsonString;
+
+	}
+	
+}
